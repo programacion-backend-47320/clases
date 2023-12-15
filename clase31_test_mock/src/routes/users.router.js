bc585d881de84f426e9801703cb9e9f122bcc61a
Arturo Verbel <arturo-verbel@hotmail.com>
2023-12-14 19:46:34 -0500

    Clase 31 - Test & Mock

---
+++ clase31_test_mock/src/routes/users.router.js
@@ -0,0 +1,16 @@
+import { Router} from 'express'
+import { generateUser } from '../utils.js'
+
+const router = Router()
+
+router.get('/', async(req, res) => {
+    const users = []
+
+    for (let i = 0; i < 3; i++) {
+        users.push(generateUser())
+    }
+
+    res.send({status: 'success', payload: users })
+})
+
+export default router
