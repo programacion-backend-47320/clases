c4d3ca3f5801a5fb84c7da794e455e43f1971e13
Arturo Verbel <arturo-verbel@hotmail.com>
2023-12-21 19:09:08 -0500

    Clase 33 - NVM & NPM

---
+++ clase33_nvm_npm/README.md
@@ -0,0 +1,3 @@
+# My Calc Library for Coderhouse
+
+Esta lireria, calcula y saluda, es una prueba para la comsion 47
---
+++ clase33_nvm_npm_use_library/index.js
@@ -0,0 +1,7 @@
+import Calc from 'clase33_nvm_npm'
+
+const calculadora = new Calc()
+
+console.log(calculadora.suma(12, 22))
+
+calculadora.saludar("Luis Eduardo Fernandez")
