987aafdbb2051c57a13ccf7c4bdcad7d2ee43636
Arturo Verbel <arturo-verbel@hotmail.com>
2024-02-15 19:52:16 -0500

    Clase 45 - Pasarela de pago

---
+++ clase45_pasarelas_pago/back/src/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import cors from 'cors'
+import paymentRouter from './routes/payments.router.js'
+
+const app = express()
+app.use(cors())
+app.use('/api/payments', paymentRouter)
+app.listen(8080, () => console.log(`Listening 🏃 ...`))
---
+++ clase45_pasarelas_pago/back/src/routes/payments.router.js
@@ -0,0 +1,35 @@
+import { Router } from 'express'
+import PaymentService from '../services/payment.service.js'
+
+const router = Router()
+
+const products = [
+    { id: 1, name: "papas", price: 1000 },
+    { id: 2, name: "queso", price: 500 },
+    { id: 3, name: "hamburguesa", price: 1500 },
+    { id: 4, name: "soda", price: 1000 },
+    { id: 5, name: "golosinas", price: 800 }
+]
+
+router.post('/payment-intents', async (req, res) => {
+    const productId = req.query.id
+    if (!productId) return res.status(400).send('no product id')
+
+    const product = products.find(p => p.id == parseInt(productId))
+    if (!product) return res.status(404).send('product not found')
+
+    const data = {
+        amount: product.price,
+        currency: 'usd',
+        payment_method_types: ['card']
+    }
+
+    const service = new PaymentService()
+    const result = await service.createPaymentIntent(data)
+
+    console.log({ result })
+
+    res.send({ status: 'success', payload: result })
+})
+
+export default router
